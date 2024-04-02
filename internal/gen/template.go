e6342b4d8668f1d0a0ac11108e47800c6bd31923
handlename <nagata@handlena.me>
2024-04-02 17:19:42 +0900

    feat: executeTemplate

--- internal/gen/template.go
+++ internal/gen/template.go
@@ -2,7 +2,13 @@ package gen
 
 import (
 	_ "embed"
+	"io"
 	"sort"
+	"strings"
+	"text/template"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 //go:embed template/service.go.tmpl
@@ -65,4 +71,30 @@ func (d *TemplateProtoRequestData) FixOrders() {
 	sort.SliceStable(d.Fields, func(i, j int) bool {
 		return d.Fields[i].Name < d.Fields[j].Name
 	})
+}
+
+func executeTemplate(data TemplateData, out io.Writer) error {
+	tmpl := template.New("service")
+	tmpl = tmpl.Funcs(template.FuncMap{
+		"PathToFuncName": func(s string) string {
+			s = strings.ReplaceAll(s, "/", "_")
+			s = strings.ReplaceAll(s, "{", "")
+			s = strings.ReplaceAll(s, "}", "")
+			return s
+		},
+		"ToCamel": func(s string) string {
+			return cases.Title(language.English).String(s)
+		},
+	})
+
+	tmpl, err := tmpl.Parse(string(ServiceTmpl))
+	if err != nil {
+		return err
+	}
+
+	if err := tmpl.ExecuteTemplate(out, "Service", data); err != nil {
+		return err
+	}
+
+	return nil
 }
--- internal/gen/template_test.go
+++ internal/gen/template_test.go
@@ -1,6 +1,7 @@
 package gen
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -130,5 +131,40 @@ func TestTemplateDataFixOrders(t *testing.T) {
 
 	if diff := cmp.Diff(original, expected); diff != "" {
 		t.Errorf("unexpected result: %v", diff)
+	}
+}
+
+func TestExecuteTemplate(t *testing.T) {
+	data := TemplateData{
+		OasPackageName:     "petv1oas",
+		ProtoServiceName:   "Pet",
+		ProtoPackagePath:   "example/gen/pet/v1",
+		ConnectPackagePath: "example/gen/pet/v1/petv1connect",
+		Endpoints: []TemplateEndpointData{
+			{
+				Path: "/pet/{petId}",
+				ProtoMethods: []TemplateProtoMethodData{
+					{
+						Name:       "UpdatePet",
+						HTTPMethod: "POST",
+						ProtoRequest: TemplateProtoRequestData{
+							Name: "",
+							Fields: []TemplateProtoFieldData{
+								{
+									Name:      "name",
+									GoType:    "string",
+									ParamType: "query",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	var buf bytes.Buffer
+	if err := executeTemplate(data, &buf); err != nil {
+		t.Fatal(err)
 	}
 }
